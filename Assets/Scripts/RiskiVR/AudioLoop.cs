f8e64a28523aa7a9d08305d57403acb89d6b0cb6
RiskiVR <riskivrmusic@gmail.com>
2024-09-04 17:57:53 -0500

    audio looping!!!!

--- Assets/Scripts/RiskiVR/AudioLoop.cs
+++ Assets/Scripts/RiskiVR/AudioLoop.cs
@@ -3,25 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class AudioLoop : MonoBehaviour
 {
-    [SerializeField] private double loopStartTime;
-    [SerializeField] private double loopEndTime;
-
-    private int loopStartSamples;
-    private int loopEndSamples;
-    private int loopLengthSamples;
- 
+    [SerializeField] private float loopStartTime;
     private AudioSource audioSource;
-    
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-        loopStartSamples = (int)(loopStartTime * audioSource.clip.frequency);
-        loopEndSamples = (int)(loopEndTime * audioSource.clip.frequency);
-        loopLengthSamples = loopEndSamples - loopStartSamples;
-    }
-
+    void Start() => audioSource = GetComponent<AudioSource>();
     void Update()
     {
-        if (audioSource.timeSamples >= loopEndSamples) { audioSource.timeSamples -= loopLengthSamples; }
+        if (audioSource.time >= audioSource.clip.length)
+        {
+            audioSource.Play();
+            audioSource.time = loopStartTime;
+        }
     }
 }
