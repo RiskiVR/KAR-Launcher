07196af491d931fe0ea4485d521e7e477afca5c6
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-09-22 22:58:32 -0400

    Replays reload data every time the menu is closed and opened

--- Assets/Scripts/RiskiVR/ReplayHandler.cs
+++ Assets/Scripts/RiskiVR/ReplayHandler.cs
@@ -57,8 +57,39 @@ public class ReplayHandler : MonoBehaviour
     [SerializeField] GameObject buttonPrefab;
     [SerializeField] Transform listParent;
     public List<Replay> replays;
-    void Awake()
+    //void Awake()
+    //{
+    //    replays = new List<Replay>();
+    //
+    //    //the replay directory
+    //    string[] replayDir = Directory.GetDirectories(System.Environment.CurrentDirectory + "/Replays");
+    //
+    //    //goes through the directories
+    //    foreach (string dir in replayDir)
+    //    {
+    //        //attempt to load the match data, if we fail, we know this Replay is invalid
+    //        Replay r = new Replay();
+    //        if (!r.LoadMatchData(dir))
+    //            continue;
+    //
+    //        //adds the Replay
+    //        replays.Add(r);
+    //    }
+    //    
+    //}
+    void OnEnable() => StartCoroutine(ListReplays());
+
+    void OnDisable()
     {
+        foreach (Transform c in listParent)
+        {
+            Destroy(c.gameObject);
+        }
+       // replays.Clear();
+    }
+
+   IEnumerator ListReplays()
+   {
         replays = new List<Replay>();
 
         //the replay directory
@@ -75,33 +106,8 @@ public class ReplayHandler : MonoBehaviour
             //adds the Replay
             replays.Add(r);
         }
-        
-    }
-    void OnEnable() => StartCoroutine(ListReplays());
 
-    //private void OnEnable()
-    //{
-    //    foreach (Replay s in replays)
-    //    {
-    //        GameObject button = Instantiate(buttonPrefab, listParent);
-    //        button.GetComponent<RectTransform>().sizeDelta = new Vector2(UnityEngine.Random.Range(-1000, 1000), 50);
-    //        button.GetComponent<ButtonInfo>().info = "Watch Replay";
-    //        button.GetComponentInChildren<TextMeshProUGUI>().text = "";
-    //    }
-    //}
-
-    void OnDisable()
-    {
-        foreach (Transform c in listParent)
-        {
-            Destroy(c.gameObject);
-        }
-        //replays.Clear();
-    }
-
-   IEnumerator ListReplays()
-   {
-       foreach (Replay s in replays)
+        foreach (Replay s in replays)
        {
            GameObject button = Instantiate(buttonPrefab, listParent);
            button.GetComponent<RectTransform>().sizeDelta = new Vector2(UnityEngine.Random.Range(-1000, 1000), 50);
